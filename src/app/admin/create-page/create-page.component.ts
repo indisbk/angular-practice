7ee2e54dd0ff1a912df55cdd6587fbb3669abadc
Цыремпилов Виктор <vtsirempilov@systematica-consulting.ru>
2020-11-09 19:21:41 +0300

    0.0.5 Post page design and refactoring css files to scss

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-page',
   templateUrl: './create-page.component.html',
-  styleUrls: ['./create-page.component.css']
+  styleUrls: ['./create-page.component.scss']
 })
 export class CreatePageComponent implements OnInit {
 
