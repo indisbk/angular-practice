eb3bb86112122820750fec89805a03ead9d67029
Цыремпилов Виктор <vtsirempilov@systematica-consulting.ru>
2020-11-13 08:19:43 +0300

    0.0.12 Added authorization guard for admin layout

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -9,14 +9,15 @@ import { EditPageComponent } from './edit-page/edit-page.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AuthService} from './shared/services/auth.service';
 import {SharedModule} from '../shared/shared.module';
+import {AuthGuard} from './shared/services/auth.guard';
 
 const routes: Routes = [
   {path: '', component: AdminLayoutComponent, children: [
       {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
       {path: 'login', component: LoginPageComponent},
-      {path: 'dashboard', component: DashboardPageComponent},
-      {path: 'create', component: CreatePageComponent},
-      {path: 'post/:id/edit', component: EditPageComponent}
+      {path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard]},
+      {path: 'create', component: CreatePageComponent, canActivate: [AuthGuard]},
+      {path: 'post/:id/edit', component: EditPageComponent, canActivate: [AuthGuard]}
     ]}
 ];
 
@@ -35,7 +36,7 @@ const routes: Routes = [
     RouterModule.forChild(routes)
   ],
   exports: [RouterModule],
-  providers: [AuthService]
+  providers: [AuthService, AuthGuard]
 })
 export class AdminModule {
 
--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
 import {AuthService} from '../shared/services/auth.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -12,14 +12,26 @@ import {Router} from '@angular/router';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+
+  // Field for disable/enable button "Login"
   submitted = false;
+
+  // Message for warning user
+  message: string;
 
   constructor(
     public auth: AuthService,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params.loginAgain) {
+        this.message = 'Please, enter login data: email and password';
+      }
+    });
+
     this.form = new FormGroup({
       email: new FormControl('', [
         Validators.required,
